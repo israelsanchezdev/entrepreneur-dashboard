e1878a9de2f79d717862c326956248171ad8dc86
israelsanchezdev <63677725+israelsanchezdev@users.noreply.github.com>
2025-05-03 09:34:41 -0500

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,9 @@
-export default {
-  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+// tailwind.config.js
+module.exports = {
+  darkMode: 'class',              // ← enable "dark:" variants via a .dark class on <html>
+  content: ["./index.html", "./src/**/*.{js,jsx}"],
   theme: {
     extend: {},
   },
   plugins: [],
-};
+}
